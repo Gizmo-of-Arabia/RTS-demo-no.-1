b4d3957c4ed94bd0728607b77baf8f203106f424
Maciej Mikusek <bombelxi@gmail.com>
2023-01-23 20:10:31 +0100

    Implement TapSelect Action
    Raycasts happen when you click, might need a different interaction, but we'll see

--- RTS-demo-no.-1/Assets/Scipts/Inputs/BattlefieldControlInputHandler.cs
+++ RTS-demo-no.-1/Assets/Scipts/Inputs/BattlefieldControlInputHandler.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal.Internal;
 
 public class BattlefieldControlInputHandler : MonoBehaviour
 {
     private Controls controls;
+
+    #region Default Methods
 
     private void Awake()
     {
@@ -14,6 +18,7 @@ public class BattlefieldControlInputHandler : MonoBehaviour
     private void OnEnable()
     {
         controls.BattlefieldControl.Enable();
+        controls.BattlefieldControl.TapSelect.performed += OnTapSelect;
     }
 
     private void Start()
@@ -21,8 +26,28 @@ public class BattlefieldControlInputHandler : MonoBehaviour
         
     }
 
+    private void Update()
+    {
+        //Debug.Log(Input.mousePosition); // TEMP
+    }
+
     private void OnDisable()
     {
         controls.BattlefieldControl.Disable();
+        controls.BattlefieldControl.TapSelect.performed -= OnTapSelect;
     }
+
+    #endregion
+    #region Custom Methods
+
+    private void OnTapSelect(InputAction.CallbackContext context)
+    {
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition))) {
+            Debug.Log("Hit a thing");
+        }
+    }
+
+
+
+    #endregion
 }
